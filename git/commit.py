fa9dd9c23eb64e50d5a4f452cc36c912b0bc54f2
Lalit Kapoor <lalitkapoor@gmail.com>
2011-06-09 14:58:00 -0500

    checking to make sure a JIRA issue exists inorder to continue

--- git/commit.py
+++ git/commit.py
@@ -22,16 +22,16 @@ commands = set()
 issueIDs = set()
 
 for match in matches:
-	match = match[1:].upper()
+	match = match[1:].upper() #remove the hash and make it all uppercase
 	if match in settings.commands:
 		print "command found:", match
 		commands.add(match)
 		#do something here, act on the command
 		#do this when the push has completed
 	else:
-		print "issue found:", match
+		print "potential JIRA issue:", match
 		#this might be an issue so add to list of issues
-		issueIDs.add(match) #remove the hash and make it all uppercase
+		issueIDs.add(match)
 
 #check to make sure all issues specified exist:
 for issueID in issueIDs:
@@ -39,13 +39,14 @@ for issueID in issueIDs:
 	
 	try:
 		issue = client.service.getIssue(auth, issueID)
+		print "JIRA issue found:", issueID
 	except WebFault as err:
-		print "Error finding issue:", (issueID)
+		print "Error finding JIRA issue:", (issueID)
 		exit(1) #exit because, if there are errors we don't want to commit
 
 exit(0)
 
-#do this only when git hook calls us on a git push	
+#do this only when the github hook calls us on a successful git push	
 for issueID in issueIDs:
-	print "adding comment for issue:", issueID
+	print "adding comment for JIRA issue:", issueID
 	client.service.addComment(auth, issueID, {'body': comment})
